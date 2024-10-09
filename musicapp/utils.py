18eab95356110ab8c30eee746ae4fbabc30cacf2
DSLightfoot <dsl38@uakron.edu>
2024-10-09 22:20:20 +0000

    Fix utils.py redirect

--- musicapp/utils.py
+++ musicapp/utils.py
@@ -1,15 +1,22 @@
 import os
 from dotenv import load_dotenv
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 
 # Load environment variables
 load_dotenv()
 
 def get_spotify_client():
-    client_credentials_manager = SpotifyClientCredentials(
-        client_id=os.getenv('SPOTIFY_CLIENT_ID'),
-        client_secret=os.getenv('SPOTIFY_CLIENT_SECRET')
-    )
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    # client_credentials_manager = SpotifyClientCredentials(
+    #     client_id=os.getenv('SPOTIFY_CLIENT_ID'),
+    #     client_secret=os.getenv('SPOTIFY_CLIENT_SECRET')
+
+    #     #Spotify client id = 4461206ef25a4ebab77c254cdf61b2bc
+    #     #Spotify client secret = SPO69401507e02f4b2fa968967ca7114f4f
+    # )
+    # sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id = '4461206ef25a4ebab77c254cdf61b2bc', 
+                                                     client_secret = 'SPO69401507e02f4b2fa968967ca7114f4f', 
+                                                     redirect_uri = 'http://localhost:8080'))
     return sp
