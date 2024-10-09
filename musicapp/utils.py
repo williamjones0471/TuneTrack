f5ded1c11b6fc02bd50cfd335f2a0fcf5dfd1125
williamjones0471 <wwj3@uakron.edu>
2024-10-09 19:56:25 +0000

    Fixed server issues and updated code

--- musicapp/utils.py
+++ musicapp/utils.py
@@ -8,8 +8,8 @@ load_dotenv()
 
 def get_spotify_client():
     client_credentials_manager = SpotifyClientCredentials(
-        client_id=os.getenv('SPOTIFY_CLIENT_ID'),
-        client_secret=os.getenv('SPOTIFY_CLIENT_SECRET')
+        client_id=os.getenv('4461206ef25a4ebab77c254cdf61b2bc'),
+        client_secret=os.getenv('SPO69401507e02f4b2fa968967ca7114f4f')
     )
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     return sp
