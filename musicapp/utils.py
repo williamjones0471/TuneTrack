dd208f9b58a1253cf4796d2a45ca65a21d89756c
DSLightfoot <dsl38@uakron.edu>
2024-10-09 23:37:30 +0000

    Update working and running front and backend

--- musicapp/utils.py
+++ musicapp/utils.py
@@ -7,27 +7,7 @@ from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 load_dotenv()
 
 def get_spotify_client():
-<<<<<<< HEAD
-    # client_credentials_manager = SpotifyClientCredentials(
-    #     client_id=os.getenv('SPOTIFY_CLIENT_ID'),
-    #     client_secret=os.getenv('SPOTIFY_CLIENT_SECRET')
-
-    #     #Spotify client id = 4461206ef25a4ebab77c254cdf61b2bc
-    #     #Spotify client secret = SPO69401507e02f4b2fa968967ca7114f4f
-    # )
-    # sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
     sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id = '4461206ef25a4ebab77c254cdf61b2bc', 
                                                      client_secret = 'SPO69401507e02f4b2fa968967ca7114f4f', 
-                                                     redirect_uri = 'http://localhost:8080'))
+                                                     redirect_uri = 'http://localhost:8080/callback/'))
     return sp
-=======
-    client_id = os.getenv('SPOTIPY_CLIENT_ID')
-    client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
-    client_credentials_manager = SpotifyClientCredentials(
-        client_id=client_id,
-        client_secret=client_secret
-    )
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    return sp
->>>>>>> refs/remotes/origin/main
--- musicapp/views.py
+++ musicapp/views.py
@@ -3,10 +3,6 @@ from .utils import get_spotify_client
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-spotify = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id = '4461206ef25a4ebab77c254cdf61b2bc',
-                                                      client_secret = '69401507e02f4b2fa968967ca7114f4f',
-                                                      redirect_uri = 'http://localhost:8000/callback/'))
-
 def home(request):
     sp = get_spotify_client()
     # Fetch general data from Spotify (e.g., search for artists)
