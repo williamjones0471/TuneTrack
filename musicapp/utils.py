b4cbc65b6624d0c2adce7626751a6229c182f848
williamjones0471 <wwj3@uakron.edu>
2024-10-09 20:20:45 +0000

    Fix syntax errors and correct Spotify environment variable usage

--- musicapp/utils.py
+++ musicapp/utils.py
@@ -7,9 +7,11 @@ from spotipy.oauth2 import SpotifyClientCredentials
 load_dotenv()
 
 def get_spotify_client():
+    client_id = os.getenv('SPOTIPY_CLIENT_ID')
+    client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
     client_credentials_manager = SpotifyClientCredentials(
-        client_id=os.getenv('4461206ef25a4ebab77c254cdf61b2bc'),
-        client_secret=os.getenv('SPO69401507e02f4b2fa968967ca7114f4f')
+        client_id=client_id,
+        client_secret=client_secret
     )
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     return sp
