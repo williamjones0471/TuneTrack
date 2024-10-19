eede6d306f5e83aa75b7f35f604849200d857b72
williamjones0471 <wwj3@uakron.edu>
2024-10-19 23:07:49 +0000

    Delete duplicate line from settings.py

--- tunetrack/settings.py
+++ tunetrack/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from pathlib import Path
 
 # Load environment variables from .env file
 # load_dotenv()
