0a1e580938d80747402498618934265da1dd2e05
DSLightfoot <dsl38@uakron.edu>
2024-10-20 01:19:30 +0000

    Add models to admin endpoint

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Artist, Album, Song, Playlist_Song, Playlist
+
+admin.site.register(User)
+admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(Song)
+admin.site.register(Playlist_Song)
+admin.site.register(Playlist)
