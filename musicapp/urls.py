4ddd37e571b4b871379cd5f375182818e1608a6e
DSLightfoot <dsl38@uakron.edu>
2024-11-22 08:41:34 +0000

    Update sign-in functionality

--- musicapp/urls.py
+++ musicapp/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('playlist/<str:playlist_id>/add-songs/', views.add_song_to_playlist, name='add_song_to_playlist'),
     path('playlist/<str:playlist_id>/', views.playlist_detail, name='playlist_detail'),
     path('playlist/<str:playlist_id>/delete_song/<str:song_id>/', views.delete_song_from_playlist, name='delete_song_from_playlist'),
+    path('login/create-account', views.create_account, name='create_account'),
+    path('login/returning-user', views.returning_user, name='returning_user'),
 ]
--- musicapp/views.py
+++ musicapp/views.py
@@ -1,194 +1,16 @@
-"""
-from django.shortcuts import render
-from .utils import get_spotify_client
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-
-def home(request):
-    sp = get_spotify_client()
-
-    # Shows all of the attributes that sp can do
-    # print(dir(sp))
-
-    # Gets username
-    user = sp.current_user()
-    user = user["display_name"]
-
-    if request.method == "POST":
-        artist = request.POST["artist"]
-        print(artist)
-
-        # Fetch general data from Spotify (e.g., search for artists)
-        results = sp.search(q=f'artist:{artist}', type='artist')
-        artists = results['artists']['items']
-
-        artist_id = artists[0]['id']
-        print(sp.artist_top_tracks(artist_id))
-
-        
-
-        return render(request, 'musicapp/home.html', {
-            'user': user,
-            'artists': artists,
-            })
-    else:
-        # sp = get_spotify_client()
-
-        # Fetch general data from Spotify (e.g., search for artists)
-        results = sp.search(q='artist:Coldplay', type='artist')
-        artists = results['artists']['items']
-        return render(request, 'musicapp/home.html', {
-            'user': user,
-            'artists': artists,
-            })
-"""
-"""
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login as auth_login
-from django.contrib.auth.models import User
-import os
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-
-def login_view(request):
-    sp_oauth = SpotifyOAuth(
-        client_id=settings.SPOTIPY_CLIENT_ID,
-        client_secret=settings.SPOTIPY_CLIENT_SECRET,
-        redirect_uri=settings.SPOTIPY_REDIRECT_URI,
-        scope='user-read-private user-read-email',
-        cache_path=None
-    )
-    auth_url = sp_oauth.get_authorize_url()
-    return redirect(auth_url)
-
-def callback(request):
-    sp_oauth = SpotifyOAuth(
-        client_id=settings.SPOTIPY_CLIENT_ID,
-        client_secret=settings.SPOTIPY_CLIENT_SECRET,
-        redirect_uri=settings.SPOTIPY_REDIRECT_URI,
-        scope='user-read-private user-read-email'
-    )
-    code = request.GET.get('code')
-    token_info = sp_oauth.get_access_token(code)
-
-    if token_info:
-        access_token = token_info['access_token']
-        request.session['access_token'] = access_token
-
-        # Fetch user profile information
-        sp = spotipy.Spotify(auth=access_token)
-        spotify_user = sp.current_user()
-
-        # Create or get the user
-        user, created = User.objects.get_or_create(
-            username=spotify_user['id'],
-            defaults={
-                'email': spotify_user.get('email', ''),
-                'first_name': spotify_user.get('display_name', '')
-            }
-        )
-        auth_login(request, user)
-
-        return redirect('home')
-    else:
-        return render(request, 'musicapp/error.html', {'message': 'Failed to authenticate with Spotify.'})
-
-@login_required
-def home(request):
-    sp_oauth = SpotifyOAuth(
-        client_id=settings.SPOTIFY_CLIENT_ID,
-        client_secret=settings.SPOTIFY_CLIENT_SECRET,
-        redirect_uri=settings.SPOTIFY_REDIRECT_URI,
-        scope='user-read-private user-read-email',
-        cache_path=None,
-    )
-
-    token_info = sp_oauth.validate_token({'access_token': request.session.get('access_token')})
-
-    if not token_info:
-        # Token is invalid or expired, refresh it
-        refresh_token = request.session.get('refresh_token')
-        if refresh_token:
-            token_info = sp_oauth.refresh_access_token(refresh_token)
-            request.session['access_token'] = token_info['access_token']
-            request.session['refresh_token'] = token_info['refresh_token']
-        else:
-            return redirect('login')
-
-    sp = spotipy.Spotify(auth=request.session['access_token'])
-
-    # Get the user's display name
-    spotify_user = sp.current_user()
-    user_display_name = spotify_user.get('display_name', 'User')
-
-    if request.method == "POST":
-        artist = request.POST.get("artist", "")
-        print(artist)
-
-        # Fetch general data from Spotify (e.g., search for artists)
-        results = sp.search(q=f'artist:{artist}', type='artist')
-        artists = results['artists']['items']
-
-        if artists:
-            artist_id = artists[0]['id']
-            top_tracks = sp.artist_top_tracks(artist_id)
-            print(top_tracks)
-
-        return render(request, 'musicapp/home.html', {
-            'user': user_display_name,
-            'artists': artists,
-        })
-    else:
-        # Fetch general data from Spotify (e.g., search for artists)
-        results = sp.search(q='artist:Coldplay', type='artist')
-        artists = results['artists']['items']
-        return render(request, 'musicapp/home.html', {
-            'user': user_display_name,
-            'artists': artists,
-        })
-
-
-from django.shortcuts import render, redirect
-from django.conf import settings
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login as auth_login
-from django.contrib.auth import get_user_model
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-from django.contrib.auth import logout
-
-User = get_user_model()
-"""
-
-from django.shortcuts import render, redirect
-from django.conf import settings
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout, login as auth_login
-from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth import logout, login
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 import spotipy
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from spotipy.oauth2 import SpotifyOAuth
+from django.core.exceptions import ObjectDoesNotExist
 
-User = get_user_model()
-
-
-
-"""
-def login_view(request):
-    sp_oauth = SpotifyOAuth(
-        client_id=settings.SPOTIFY_CLIENT_ID,
-        client_secret=settings.SPOTIFY_CLIENT_SECRET,
-        redirect_uri=settings.SPOTIFY_REDIRECT_URI,
-        scope='user-read-private user-read-email',
-        cache_path=None
-    )
-    auth_url = sp_oauth.get_authorize_url()
-    return redirect(auth_url)
-"""
+from .models import User
 
 def login_view(request):
     return render(request, 'musicapp/login.html')
@@ -218,58 +40,103 @@ def callback(request):
 
         # Store the entire token_info in the session
         request.session['token_info'] = token_info
-
         access_token = token_info['access_token']
-
-        # Fetch user profile information
         sp = spotipy.Spotify(auth=access_token)
         spotify_user = sp.current_user()
+        user_display_name = spotify_user.get('display_name', 'User')
 
-        # Create or get the user
-        user, created = User.objects.get_or_create(
-            username=spotify_user['id'],
-            defaults={
-                'email': spotify_user.get('email', ''),
-                'first_name': spotify_user.get('display_name', '')
-            }
-        )
-        auth_login(request, user)
+        try:
+            user = User.objects.get(username=user_display_name)
 
-        return redirect('home')
-    else:
-        return render(request, 'musicapp/error.html', {'message': 'Failed to authenticate with Spotify.'})
+            print(user, 'USERDSF:JKL')
+            if user:
+                return HttpResponseRedirect(reverse('returning_user'))
+        except ObjectDoesNotExist:
+            return HttpResponseRedirect(reverse('create_account'))
+        else:
+            return render(request, 'musicapp/error.html', {'message': 'Failed to authenticate with Spotify.'})
+    
+def create_account(request):
+    token_info = request.session.get('token_info')
+    access_token = token_info.get('access_token')
+    sp = spotipy.Spotify(auth=access_token)
+    spotify_user = sp.current_user()
+    user_display_name = spotify_user.get('display_name', 'User')
+    
+    if not token_info:
+        #Redirect to the login page 
+        return redirect('spotify_login')
 
-    """
-    token_info = sp_oauth.get_access_token(code)
+    if request.method == "POST":
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        password = request.POST['password']
+        confirmation = request.POST['confirmation']
 
-    if token_info:
-        # Store the entire token_info in the session
-        request.session['token_info'] = token_info
+        if password != confirmation:
+            return render(request, 'musicapp/create_account.html', {
+                "message": "Passwords must match."
+            })
 
-        access_token = token_info['access_token']
+        user = User.objects.create_user(username=spotify_user['display_name'], password=password, first_name=firstname, last_name=lastname, email=spotify_user.get('email'))
+        user.save()
 
-        # Fetch user profile information
-        sp = spotipy.Spotify(auth=access_token)
-        spotify_user = sp.current_user()
+        user_auth = authenticate(request, username=user_display_name, password=password)
+        print(user, user_auth)
 
-        # Create or get the user
-        user, created = User.objects.get_or_create(
-            username=spotify_user['id'],
-            defaults={
-                'email': spotify_user.get('email', ''),
-                'first_name': spotify_user.get('display_name', '')
-            }
-        )
-        auth_login(request, user)
+        if user_auth is not None:
+            login(request, user_auth)
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return render(request, 'musicapp/create_account.html', {
+                "message": "Authentication Error"
+            })
+    
+    return render(request, 'musicapp/create_account.html')
 
-        return redirect('home')
-    else:
-        return render(request, 'musicapp/error.html', {'message': 'Failed to authenticate with Spotify.'})
-"""
+def returning_user(request):
+    token_info = request.session.get('token_info')
+    access_token = token_info.get('access_token')
+    sp = spotipy.Spotify(auth=access_token)
+    spotify_user = sp.current_user()
+    user_display_name = spotify_user.get('display_name', 'User')
+
+    if not token_info:
+    #Redirect to the login page 
+        return redirect('spotify_login')
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+
+        try:
+            user = User.objects.get(username=user_display_name)
+        except ObjectDoesNotExist:
+            return render(request, 'musicapp/create_account.html', {
+                "message": "User does not exist."
+            })
+    
+        user = authenticate(request, username=username, password=password)
+        print(user)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return render(request, 'musicapp/returning_user.html', {
+                "message": "Invalid username and/or password"
+            })
+
+    
+    return render(request, 'musicapp/returning_user.html', {
+                    'user': user_display_name
+                })
+
 @login_required
 def home(request):
+    token_info = request.session.get('token_info')
 
-    token_info = request.session.get('token_info', None)
     if not token_info:
         return redirect('login')
     
@@ -335,47 +202,6 @@ def home(request):
         'artists': artists,
     })
 
-"""
-    # Check if token is expired
-    if sp_oauth.is_token_expired(token_info):
-        # Refresh the token
-        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
-        # Update the session with the new token_info
-        request.session['token_info'] = token_info
-
-    access_token = token_info['access_token']
-    sp = spotipy.Spotify(auth=access_token)
-
-    # Get the user's display name
-    spotify_user = sp.current_user()
-    user_display_name = spotify_user.get('display_name', 'User')
-
-    if request.method == "POST":
-        artist = request.POST.get("artist", "")
-        print(artist)
-
-        # Fetch general data from Spotify (e.g., search for artists)
-        results = sp.search(q=f'artist:{artist}', type='artist')
-        artists = results['artists']['items']
-
-        if artists:
-            artist_id = artists[0]['id']
-            top_tracks = sp.artist_top_tracks(artist_id)
-            print(top_tracks)
-
-        return render(request, 'musicapp/home.html', {
-            'user': user_display_name,
-            'artists': artists,
-        })
-    else:
-        # Fetch general data from Spotify (e.g., search for artists)
-        results = sp.search(q='artist:Coldplay', type='artist')
-        artists = results['artists']['items']
-        return render(request, 'musicapp/home.html', {
-            'user': user_display_name,
-            'artists': artists,
-        })
-"""
 def logout_view(request):
     # Clear the session data
     request.session.flush()
